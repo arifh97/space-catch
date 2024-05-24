f8f7fa020609e3ed13bc5965954e539ee978245d
imranhwebdev <alimrancv870@gmail.com>
2024-05-24 18:21:04 +0600

    about and tokenomics 90% done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
-
+import React from "react";
+import {Container, Col, Row } from 'react-bootstrap';
+import aboutImg from "../assets/img/aboutImg.png"
 export default function About() {
+  const aboutContent = [
+    {
+      title : "AUGMENTED REALITY",
+      desc: "Advanced AR combat mode for the best gaming experience!",
+    },
+    {
+      title : "AI, MOVE-TO-EARN, WEB3",
+      desc: "Complex AI features, sustainable move-to-earn. Trade your assets!",
+    },
+    {
+      title : "PROFESSIONAL GAMING STUDIO",
+      desc: "Designed by the professional gaming studio: Pixelfield",
+    },
+  ]
   return (
-    <div>About</div>
-  )
+    <div className="about" id='about'>
+      <Container>
+        <Row className="section-title text-center text-lg-start">
+          <Col lg={6}>
+            <h2>SPACECATCH THE NEXT-GENERATION AR GAME</h2>
+          </Col>
+          <Col lg={6}>
+            <p>Introducing SPACECATCH, the cutting-edge AR gaming experience poised to revolutionize how we interact with the virtual world. </p>
+          </Col>
+        </Row>
+        <Row className='justify-content-center'>
+          <Col lg={6} className="order-2 order-lg-1">
+            <div className="about-left">              
+              <figure className="aboutImg">
+                <img src={aboutImg} alt="" />
+              </figure>
+            </div>
+          </Col>
+          <Col lg={6} className="order-1 order-lg-2 text-center text-lg-start">
+            <div className="about-content">
+              {aboutContent.map((item, index)=>(
+                <div className="single-item" key={index}>
+                  <h4>{item.title}</h4>
+                  <p>{item.desc}</p>
+                </div>
+              ))}
+              </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -5,29 +5,35 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 import tokenImg from '../assets/img/token-img.png'
+import tokenContentBG from '../assets/img/tokenContentBG.png'
 import CommonTitle from "./CommonTitle";
 
 export default function tokenomics() {  
   const tokenData = [
     {
-      name: 'Lp',
-      percent: 70,
-      color: '#E16DDF',
+      name: 'Liquidity',
+      percent: 35,
+      color: '#E5CF63',
     },
     {
-      name: 'Cex listing',
-      percent: 10,
-      color: '#FFD41C',
+      name: 'Team',
+      percent: 5,
+      color: '#C060A1',
     },
     {
-      name: 'Marketing ',
+      name: 'Future development ',
       percent: 10,
-      color: '#FB7C28',
+      color: '#0147F7',
     },
     {
-      name: 'Airdrop',
-      percent: 10,
-      color: '#023FD5',
+      name: 'Staking  rewards',
+      percent: 20,
+      color: '#2BBF7F',
+    },
+    {
+      name: 'Presale',
+      percent: 30,
+      color: '#6D67E4',
     },
   ];
 
@@ -42,7 +48,7 @@ export default function tokenomics() {
         borderWidth: 0,
         hoverOffset: 0,
         borderRadius:0,
-        cutout:100,
+        cutout:"70%"
       },
     ],
   };
@@ -66,23 +72,28 @@ export default function tokenomics() {
   return (
     <div className='tokenomics common-padding'>
       <Container>
-        <Row className=' align-items-center'>
-          <Col xs={12} md={6} >
-            <CommonTitle className="text-center text-md-start mb-4 mb-md-5" title="Tokenomics" />            
+        <Row className='tokenomics-wrapper align-items-center'>
+          <figure className="tokenContentBG">
+            <img src={tokenContentBG} alt="" />
+          </figure>
+          <Col lg={6} >
+            <h2>Tokeno<span>mics</span></h2>       
             <div className="tokenomics-percentage">
               {tokenData.map((item, index) => (
                 <div className="tokenomics-percentage-item" key={index} data-aos="fade-up" data-aos-duration={`1${index+6}00`}>
-                  <div className="line">
-                    <div className="line-wrap" style={{ backgroundColor: item.color }}></div>
+                  <div className="dot"  style={{ backgroundColor: item.color }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="15" viewBox="0 0 22 15" fill="none">
+                      <path d="M21.6853 1.27821L7.89717 14.768L0.712891 7.73316L1.87373 6.5985L7.89717 12.4735L20.5245 0.143555L21.6853 1.27821Z" fill="white"/>
+                    </svg>
                   </div>
                   <div className="text">
-                    <p>{item.percent}% {item.name}</p>
+                    <p>{item.name}  {item.percent}% </p>
                   </div>
                 </div>
               ))}
             </div>
           </Col>
-          <Col xs={12} md={6} className='d-flex justify-content-center justify-content-md-end'>
+          <Col lg={6} className='d-flex justify-content-center justify-content-md-end'>
             <div className="tokenomics-chart position-relative z-1" data-aos="fade-left">
               <div className="tokenomics-logo position-absolute top-50 start-50 z-n1">
                 <img src={tokenImg} alt="" />
